94f4f49bb9be8fb9ccee1485676ae1b938a31b00
LiPengfei19820619 <22201846@qq.com>
2017-07-24 00:57:50 +0800

    add create for adjnode.service

--- src/app/topoly/adjnode.service.ts
+++ src/app/topoly/adjnode.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Headers, Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 
@@ -9,13 +9,19 @@ import { Adjnode } from './Adjnode';
 
 @Injectable()
 export class AdjnodeService {
+  private headers = new Headers({'Content-Type': 'application/json'});
   private adjnodesUrl = 'http://localhost:9090/api/v1/adjnodes';  // URL to web api
 
   constructor(private http: Http) { }
 
   getAdjnodes(): Observable<Adjnode[]> {
     return this.http.get(this.adjnodesUrl)
-                    .map(this.extractData)
+                    .map(this.extractData);
+  }
+
+  create(node: Adjnode): Observable<Adjnode> {
+    return this.http
+        .post(this.adjnodesUrl, JSON.stringify(node), {headers: this.headers});
   }
 
   extractData(result: Response): Adjnode[] {
