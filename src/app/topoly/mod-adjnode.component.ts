a18b78a37823e21a0ade99cd1326d2f5a66e694e
LiPengfei19820619 <22201846@qq.com>
2017-07-28 01:38:01 +0800

    修改邻接节点界面

--- src/app/topoly/mod-adjnode.component.ts
+++ src/app/topoly/mod-adjnode.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Router } from '@angular/router';
 import { Location }                 from '@angular/common';
 
 import { Adjnode } from './adjnode';
@@ -14,13 +16,18 @@ import 'rxjs/add/operator/switchMap';
   styleUrls: ['./mod-adjnode.component.css']
 })
 export class ModAdjnodeComponent implements OnInit {
-  node: Adjnode
+  @Input() node: Adjnode
+
+  adjnodeForm: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     private adjnodeService: AdjnodeService,
     private route: ActivatedRoute,
     private location: Location
-  ) { }
+  ) {
+    this.createForm();
+  }
 
   ngOnInit(): void {
     this.route.paramMap
@@ -29,7 +36,30 @@ export class ModAdjnodeComponent implements OnInit {
                 alert("id:"+id);
                 return this.adjnodeService.getAdjnode(+id);
               } )
-              .subscribe(node => this.node = node);
+              .subscribe(node => {
+                this.node = node;
+                this.adjnodeForm.setValue({
+                  id: this.node.id,
+                  ipaddr: this.node.ipaddr,
+                  ipver: this.node.ipver,
+                  port: this.node.port,
+                  hostname: this.node.hostname,
+                  domain: this.node.domain,
+                  type: this.node.type
+                })
+              });
+  }
+
+  createForm() {
+    this.adjnodeForm = this.fb.group({
+      id : 0,
+      ipaddr : '',
+      ipver : 4,
+      port : 0,
+      hostname : '',
+      domain : '',
+      type : 1
+    });
   }
 
   goBack(): void {
