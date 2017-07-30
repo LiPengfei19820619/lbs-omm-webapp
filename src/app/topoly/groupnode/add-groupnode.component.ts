f65d75cc5db693cac4bf62181c8903079d5f980f
LiPengfei19820619 <22201846@qq.com>
2017-07-30 22:56:27 +0800

    增加节点组内的节点

--- src/app/topoly/groupnode/add-groupnode.component.ts
+++ src/app/topoly/groupnode/add-groupnode.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { Groupnode } from './groupnode';
+import { GroupnodeService } from './groupnode.service';
 
 @Component({
   selector: 'app-add-groupnode',
   templateUrl: './add-groupnode.component.html',
   styleUrls: ['./add-groupnode.component.css']
 })
-export class AddGroupnodeComponent implements OnInit {
+export class AddGroupnodeComponent implements OnChanges {
+  @Input() groupnode: Groupnode;
+  
+  groupnodeForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private groupnodeService: GroupnodeService,
+    private route: Router,
+    private location: Location) {
 
-  ngOnInit() {
+      this.createForm();
   }
 
+  createForm() {
+    this.groupnodeForm = this.fb.group({
+      groupid : 0,
+      nodeid : 0
+    });
+  }
+
+  ngOnChanges() {
+  }
+
+  onSubmit() {
+    this.groupnode = this.groupnodeForm.value;
+    alert("submit, nodeid:" + JSON.stringify(this.groupnode));
+    this.groupnodeService.create(this.groupnode)
+                         .subscribe(result => {
+                                      alert("result:" + JSON.stringify(result));
+                                      this.route.navigate(['groupnodes']);
+                                    },
+                                    err => alert(err.message));
+  }
+
+  cancel(): void {
+    this.location.back();
+  }
 }
