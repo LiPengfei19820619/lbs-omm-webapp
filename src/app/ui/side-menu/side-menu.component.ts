0fac9970eec3879d9bcc18950d95e2c9872678c9
LiPengfei19820619 <22201846@qq.com>
2017-08-28 22:28:17 +0800

    修改侧边菜单样式

--- src/app/ui/side-menu/side-menu.component.ts
+++ src/app/ui/side-menu/side-menu.component.ts
@@ -20,7 +20,7 @@ export class SideMenuComponent implements OnInit {
                                         {url: "/", title: "修改管控基本属性配置", tooltip: "修改安全管控基本属性", hidden: false, children: null},
                                         {url: "/", title: "删除管控基本属性配置", tooltip: "删除安全管控基本属性", hidden: false, children: null}
                                  ]},
-                                 {url: "/", title: "安全管控节点配置", tooltip: "配置安全管控节点", hidden: false, children: [
+                                 {url: null, title: "安全管控节点配置", tooltip: "配置安全管控节点", hidden: false, children: [
                                         {url: "/adjnodes", title: "查看安全管控节点配置", tooltip: "查看安全管控节点", hidden: false, children: null},
                                         {url: "/", title: "增加管控节点配置", tooltip: "增加安全管控节点", hidden: false, children: null},
                                         {url: "/", title: "修改管控节点配置", tooltip: "修改安全管控节点", hidden: false, children: null},
@@ -32,7 +32,7 @@ export class SideMenuComponent implements OnInit {
                                    {url: "/securityctrl-property", title: "查看NetApi网关基本属性配置", tooltip: "查看NetApi网关基本属性", hidden: false, children: null},
                                    {url: "/", title: "修改NetApi网关基本属性配置", tooltip: "修改NetApi网关基本属性", hidden: false, children: null}
                             ]},
-                            {url: "/", title: "NetApi节点配置", tooltip: "配置NetApi节点", hidden: false, children: [
+                            {url: null, title: "NetApi节点配置", tooltip: "配置NetApi节点", hidden: false, children: [
                                    {url: "/adjnodes", title: "查看NetApi节点配置", tooltip: "查看NetApi节点", hidden: false, children: null},
                                    {url: "/", title: "增加NetApi节点配置", tooltip: "增加NetApi节点", hidden: false, children: null},
                                    {url: "/", title: "修改NetApi节点配置", tooltip: "修改NetApi节点", hidden: false, children: null},
